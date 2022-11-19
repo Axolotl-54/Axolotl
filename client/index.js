f5c988f4dc7790503eaa209275611d33d0fc3ee5
Sbrown2018 <sara.brown07.sb@gmail.com>
2022-11-18 20:11:02 -0800

    Added more to my comment in /client/index.js

--- client/index.js
+++ client/index.js
@@ -3,7 +3,7 @@ import  ReactDOM  from "react-dom/client";
 import App from "./components/app";
 import { createRoot } from 'react-dom/client';
 
-// Nu?
+// Nu? Did this got through?
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
